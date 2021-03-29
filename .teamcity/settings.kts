475aba4431893cba5845f5963ffca16043704ba9
Jordan Bergin <jordan.j.bergin@protonmail.com>
2021-03-29 20:54:49 +0100

    Trying out difference location for jacoco classLocations

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,28 +2,6 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2020.2"
 
 project {
@@ -60,7 +38,7 @@ object Build : BuildType({
             tasks = "build"
             jdkHome = "%env.JDK_11_0_x64%"
             coverageEngine = jacoco {
-                classLocations = "+:src/main/kotlin/**"
+                classLocations = "+:build/classes/kotlin/main/**"
                 jacocoVersion = "%teamcity.tool.jacoco.0.8.4%"
             }
         }
