a37ef36250907250c9b42aaadab30df7b6da326d
Jordan Bergin <contact@mrbergin.dev>
2021-08-08 18:22:29 +0100

    upping to Forkhandles 1.10.2.0 and Kotlin 1.5.21

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     val java = "8"
-    val kotlin = "1.5.10"
-    val forkhandles = "1.10.1.0"
+    val kotlin = "1.5.21"
+    val forkhandles = "1.10.2.0"
     val kotest = "4.6.0"
     val junit = "5.7.2"
     val hamkrest = "1.8.0.1"
