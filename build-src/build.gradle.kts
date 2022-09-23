1ae0a7611ff23487d277b9efb65fc83d83d188ff
Jordan Bergin <jordan.j.bergin@protonmail.com>
2022-09-23 23:33:05 +0100

    Added detekt gradle plugin
    This will run as part of `check` automatically

--- build-src/build.gradle.kts
+++ build-src/build.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
     implementation(libs.kotlin.gradle.plugin)
     implementation(libs.dokka.gradle.plugin)
+    implementation(libs.detekt.gradle.plugin)
 }
