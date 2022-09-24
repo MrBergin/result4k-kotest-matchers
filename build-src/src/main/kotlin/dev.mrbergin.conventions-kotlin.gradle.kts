6ada27a468c975680a7ab6a68741d69e064400b2
Jordan Bergin <jordan.j.bergin@protonmail.com>
2022-09-24 22:37:37 +0100

    Fixed a bug where consumers using Java 8 wouldn't work
    This change:
    
    * Carries on building using Java 17
    * Makes Kotlin target Java 8
    * Makes Java release target Java 8
    
    The above hopefully means that the build will carry on using Java 17 for everything, but the resulting Jar will still run on an older JDK.
    
    This at least works on the sample project when running that on Java 8

--- build-src/src/main/kotlin/dev.mrbergin.conventions-kotlin.gradle.kts
+++ build-src/src/main/kotlin/dev.mrbergin.conventions-kotlin.gradle.kts
@@ -1,5 +1,6 @@
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.accessors.dm.LibrariesForLibs
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm")
@@ -9,10 +10,20 @@ val libs = the<LibrariesForLibs>()
 
 kotlin {
     jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(libs.versions.java.get()))
+        languageVersion.set(JavaLanguageVersion.of(libs.versions.kotlin.jvm.build.get()))
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = libs.versions.kotlin.jvm.target.get()
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.release.set(libs.versions.java.release.target.get().toInt())
 }
